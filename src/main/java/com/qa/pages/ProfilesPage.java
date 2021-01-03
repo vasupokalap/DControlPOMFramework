6843f3108a2cc6ed8616ede33edd6bf63c155dec
Vasu Pokala <76841632+vasupokalap@users.noreply.github.com>
2021-01-03 14:36:31 +0530

    Update ProfilesPage.java

--- src/main/java/com/qa/pages/ProfilesPage.java
+++ src/main/java/com/qa/pages/ProfilesPage.java
@@ -1,9 +1,15 @@
 package com.qa.pages;
 
 public class ProfilesPage {
+	//By Locators
+	
 	
 	public ProfilesPage() {
 		System.out.println("Profiles Page");
+	}
+	
+	public void verifyAddProfile(){
+		
 	}
 
 }
